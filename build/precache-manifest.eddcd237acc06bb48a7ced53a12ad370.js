d43cf0926f836c317b1c9bac0fd9f7ac9df77ab7
Libaan <libaan.hassan@outlook.com>
2022-02-09 09:24:04 +0000

    firebase1.8

---
+++ build/precache-manifest.eddcd237acc06bb48a7ced53a12ad370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e4a8bb3f631a31e7743",
+    "url": "/static/css/main.c07093cd.chunk.css"
+  },
+  {
+    "revision": "7e4a8bb3f631a31e7743",
+    "url": "/static/js/main.7e4a8bb3.chunk.js"
+  },
+  {
+    "revision": "1d58f25d8ca068648f68",
+    "url": "/static/js/1.1d58f25d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9700c594e8a9f1ae57e548b14c62048e",
+    "url": "/static/media/flogo-HexRBG-Wht-72.9700c594.svg"
+  },
+  {
+    "revision": "5cff78e44ca42e5026f832f7b41fb018",
+    "url": "/static/media/github.5cff78e4.svg"
+  },
+  {
+    "revision": "68288b82eca71fea9504a0c67a116246",
+    "url": "/index.html"
+  }
+];
